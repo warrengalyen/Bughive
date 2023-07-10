0708a1595f97788126b035b44e84d28323562a34
Warren Galyen <wgalyen@hotmail.com>
2023-07-10 14:35:14 -0700

    Initial work on issue updates.

--- client/src/main/MainPage.tsx
+++ client/src/main/MainPage.tsx
@@ -48,17 +48,13 @@ const ContentPaneLayout = styled.section`
 export class MainPage extends React.Component<RouteComponentProps<{}>> {
     private viewContext = new ViewContext();
 
-    // private memberships: Memberships;
-
     public componentWillMount() {
-        // this.memberships = new Memberships();
         if (!session.isLoggedIn) {
             session.resume(this.props.location, this.props.history);
         }
     }
 
     public componentWillUpdate() {
-        // this.memberships.release();
         if (!session.isLoggedIn) {
             session.resume(this.props.location, this.props.history);
         }
